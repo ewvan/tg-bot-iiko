90daea6b7648bc37a743b4ed1dc446049f87dd3a
Gaifutdinov Ivan <eiwvan@yahoo.com>
2023-05-22 23:33:20 +0300

    test server

--- service/now.py
+++ service/now.py
@@ -61,7 +61,7 @@ class Now:
         data = {
             "revenue": revenue_sum,
             "check_count": check_count,
-            "average_check": revenue_sum / check_count
+            "average_check": revenue_sum / check_count if check_count != 0 : 0
         }
 
         return data
